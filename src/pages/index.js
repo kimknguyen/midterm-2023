9b4c064bce0e507281fcc6e9f27859ff1d572467
Kim Nguyen <kkn2011@nyu.edu>
2023-10-30 01:47:52 -0400

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import "../app/globals.css";
+import Link from "next/link"; 
+
+import ZodiacSign from "../app/components/Zodiac"; 
 import React, { useEffect, useState } from "react";
 
 const apiURL = "https://binaryjazz.us/wp-json/genrenator/v1/genre/12";
@@ -24,6 +27,9 @@ function fetchDataFromJSONFile() {
     });
 }
 
+
+
+
 export default function Home() {
   const [apiData, setApiData] = useState(null);
   const [jsonData, setJsonData] = useState(null);
@@ -46,17 +52,75 @@ export default function Home() {
       });
   }, []);
 
+ 
+
+
+
+
   if (!apiData || !jsonData) return null;
 
   return (
-    <>
-      <main>
+     <>
+     <main>
         <h1>Pick your Astrology</h1>
+        <Link href="/Aquarius">Aquarius</Link>
+        <Link href="/Aquarius">Aquarius</Link>
+        <Link href="/Aquarius">Aquarius</Link>
+        <Link href="/Aquarius">Aquarius</Link>
+        <Link href="/Aquarius">Aquarius</Link>
+        <Link href="/Aquarius">Aquarius</Link>
+        <Link href="/Aquarius">Aquarius</Link>
+        <Link href="/Aquarius">Aquarius</Link>
+        <Link href="/Aquarius">Aquarius</Link>
+        <Link href="/Aquarius">Aquarius</Link>
+        <Link href="/Aquarius">Aquarius</Link>
+        <Link href="/Aquarius">Aquarius</Link>
+        <ZodiacSign
+          zodiacAquariusElement={jsonData.western_zodiac.Aquarius.element}
+          zodiacAquariusGloss={jsonData.western_zodiac.Aquarius.gloss}
+          zodiacAquariusSymbol={jsonData.western_zodiac.Aquarius.unicode_symbol}
+          zodiacAquariusStartDate={jsonData.western_zodiac.Aquarius.approximate_start_date}
+          zodiacAquariusEndDate={jsonData.western_zodiac.Aquarius.approximate_end_date}
+
+        />
         
+       
+       
+        
+       
+       
+        
+        
+      
       </main>
-    </>
-  );
-}
+     
+     
+     
+     </>
+
+  ); 
+
+} 
+    
+   
+    
+/*
+<ZodiacSign
+          zodiacAquariusElement={jsonData.western_zodiac.Aquarius.element}
+          zodiacAquariusGloss={jsonData.western_zodiac.Aquarius.gloss}
+          zodiacAquariusSymbol={jsonData.western_zodiac.Aquarius.unicode_symbol}
+          zodiacAquariusStartDate={jsonData.western_zodiac.Aquarius.approximate_start_date}
+          zodiacAquariusEndDate={jsonData.western_zodiac.Aquarius.approximate_end_date}
+
+        />
+*/
+      
+      
+       
+      
+    
+
+
 
 
 
@@ -65,91 +129,29 @@ export default function Home() {
 
 
 /*
+<ZodiacSign
+          zodiacSigns={jsonData.western_zodiac}
 
-import "../app/globals.css" 
-import React from "react";
-
-const apiURL = "https://binaryjazz.us/wp-json/genrenator/v1/genre/12";
-const jsonFileURL = "https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/zodiac.json";
-
-
-function fetchDataFromAPI() {
-  return fetch(apiURL)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Failed to fetch data from API");
-      }
-      return response.json();
-    });
-}
-
-function fetchDataFromJSONFile() {
-  return fetch(jsonFileURL)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Failed to fetch data from JSON file");
-      }
-      return response.json();
-    });
-}
-
-
-Promise.all([fetchDataFromAPI(), fetchDataFromJSONFile()])
-.then(data => {
-  const apiData = data[0];
-  const jsonData = data[1];
-
-  // Log the data to the console
-  console.log("Data from API:", apiData);
-  console.log("Data from JSON file:", jsonData);
-
-
-})
-.catch(error => {
-  console.error("Error:", error);
-});
-
-
-
-export default function Home({ apiData, jsonData }) {
-    console.log( apiData); 
-    console.log( jsonData ); 
-
-    if (!jsonData, !apiData) return null; 
-
-    return (
-        <>
-      
-        <main>
-            <h1>Pick your Astrology</h1>
-            <Zodiac
-                zodiacAquarius ={apiData.western_zodiac.Aquarius}
-                zodiacAries ={apiData.western_zodiac.Aries}
-
-
-
-            />
+        />
 
 
 
 
-        </main>
+<main>
+        <h1>Pick your Astrology</h1>
         
-        </>
-    )
-}
+        <Zodiac 
+          zodiacSigns={jsonData.western_zodiac}
+        />
 
+        
 
-
+      
+      
+       
+      </main>
 
 */
-
-
-
-
-
-
-
 
 
 
